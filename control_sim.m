838ef00479de59c1853e1b68be469cc2ab44bc5e
Andrew Davies <andavies@cs.washington.edu>
2013-02-12 13:56:10 -0800

    Changed control_sim to only compensate for joint angles.

--- control_sim.m
+++ control_sim.m
@@ -1,8 +1,7 @@
 %%% Constants
 
-k1 = 75;
-k2 = 10;
-feet = 10;  % foot compensation factor.
+k1 = 42;
+k2 = 22;
 
 %%%%%%%%%%%%%
 
@@ -12,22 +11,19 @@ m = mj('getmodel');
 mj('reset');
 
 % Grab initial position (so we can stand still).
+f = zeros(9, 1);  % default external force (nothing).
 mj('kinematics');
 [q_stand, y_stand] = mj('get', 'qpos', 'geom_xpos');
-for i = 1:10000
+for i = 1:1000
     mj('kinematics');
     [q,v,x] = mj('get','qpos','qvel','geom_xpos');
-    
+
     %----------------
-    %
-    %  Simple Angle Compensation.  Left and right foot
-    %  are weighted to account for the upper body.
-    %
+    % Compensate for any changes in q_stand by some amount.
+    % only applies forces to joints.
     %----------------
-    f = k1 *  (q_stand - q) - k2 * v;
-    f(12) = k1 * 10 * (q_stand(12) - q(12)) - k2 * 10 * v(12);
-    f(9) = k1 * 10 * (q_stand(9) - q(9)) - k2 * 10 * v(9);
+    f(4:9) = k1 *  (q_stand(4:9) - q(4:9)) - k2 * v(4:9);
     mj('set','qfrc_external', f);
-    mj('step', 30);
+    mj('step', 15);
     mjplot;
 end
