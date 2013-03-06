66d7533c50d9273ed9af681c19faf7485b741dea
tarlc <tarlcoufal@gmail.com>
2013-03-06 00:23:49 -0800

    test

--- states.m
+++ states.m
@@ -6,7 +6,25 @@ classdef states
     properties (Constant)
         
             % Target stand angles
-            STAND_TARGET = zeros(19,1);
+            STAND_TARGET = [0.0000 
+                            0.0000
+                            0.0000 
+                           -0.5498 
+                            1.4769 
+                           -0.5236 
+                            0.0000 
+                            0.0000 
+                            0.0000
+                            0
+                            0
+                            0
+                            0
+                            0
+                            0
+                            0.4163
+                            0.1963 
+                            0
+                            0];
             
             % Target swing angles
             SWING_RIGHT_TARGET = [0.0000 
